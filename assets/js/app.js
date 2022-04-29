4f6d2413d22e84371b6b1e0aabc21e5fc17e01e3
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-04-29 16:18:32 +0200

    end_commit

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+
+        emails: [],
+
+    },
+    methods: {},
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                this.emails.push(response.data.response)
+            })
+        }
+
+    },
+
+});
